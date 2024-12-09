2dee2f99d3b1887342ce85053b07233190bf42cf
Anna shabbagh <afreenshabbagh@gmail.com>
2024-12-09 14:42:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,9 +5,9 @@
 // Questions data
 const questions = [
   {
-    question: "What is 2 + 2?",
-    choices: ["3", "4", "5"],
-    answer: "4",
+    question: "What is the capital of France?",
+    choices: ["Paris", "Newyork, "london"],
+    answer: "Paris",
   },
   {
     question: "Which language is used for web development?",
